b7476f716cb143ea9ef554909dec10f7082643f8
awb99 <andreas.wolfgang.bauer@gmail.com>
2017-02-06 12:16:19 +0100

    shipping methods added

--- index.js
+++ index.js
@@ -127,7 +127,12 @@ function orderUpdate (redstagOrderId, shippingAddress, orderAdditionalData, call
         }
         else {
             console.log("orderUpdate JSON: " + JSON.stringify(res));
-            callback (false, res.result);
+
+            if (res.error === undefined) {
+                callback (false, res.result);
+            } else {
+                callback (true, res.error.message);
+            }
         }
     });
 }
@@ -196,6 +201,42 @@ fedex.readCSV ( () => { console.log("fedex CSV data loaded."); } );
 module.exports.readCSV = fedex.readCSV;
 module.exports.fedexCost = fedex.fedexCost;
 
+// shipping methods
+
+var shippingMethodsFEDEX = [
+    "fedex_FEDEX_2_DAY",    // FedEx 2Day®
+    "fedex_FEDEX_2_DAY_AM", //	FedEx 2Day® A.M.
+    "fedex_FEDEX_EXPRESS_SAVER", // 	FedEx Express Saver®
+    "fedex_FEDEX_GROUND", //	FedEx Ground®
+    "fedex_INTERNATIONAL_ECONOMY", //	FedEx International Economy®
+    "fedex_INTERNATIONAL_PRIORITY", //	FedEx International Priority®
+    "fedex_FIRST_OVERNIGHT" ,	// FedEx First Overnight®
+    "fedex_GROUND_HOME_DELIVERY", //	FedEx Home Delivery®
+    "fedex_PRIORITY_OVERNIGHT", //	FedEx Priority Overnight®
+    "fedex_STANDARD_OVERNIGHT", // FedEx Standard Overnight®
+    "fedex_SMART_POST", //
+];
+
+var shippingMethodsUPS = [
+    "ups_01", //	UPS Next Day Air
+    "ups_02", // 	UPS Second Day Air
+    "ups_03", // 	UPS Ground
+    "ups_12", // 	UPS Three-Day Select
+    "ups_14", //	UPS Next Day Air Early A.M.
+    "ups_59", //	UPS Second Day Air A.M.
+];
+
+var shippingMethodsUSPS = [ // available via stamps.com
+    "usps_US-PM", // 	USPS Priority Mail
+    "usps_US-PMI" //	USPS Priority Mail International
+];
+
+var shippingMethodsEXTERNAL = [
+    "external_ltl" // LTL
+];
+
+var shippingMethodsAVAILABLE = shippingMethodsFEDEX.concat (shippingMethodsEXTERNAL);
+
 
 // Export ***************************************************
 
@@ -213,3 +254,10 @@ module.exports.queryAllShipments = queryAllShipments;
 
 // Inventory
 module.exports.queryInventory = queryInventory;
+
+// Shipping Methods
+module.exports.shippingMethodsFEDEX = shippingMethodsFEDEX;
+module.exports.shippingMethodsUPS = shippingMethodsUPS;
+module.exports.shippingMethodesUSPS = shippingMethodsUSPS;
+module.exports.shippingMethodsEXTERNAL = shippingMethodsEXTERNAL;
+module.exports.shippingMEthodsAVAILABLE = shippingMethodsAVAILABLE;
