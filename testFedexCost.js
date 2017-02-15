fa94ca7c5bec9f6056086efae7ac83ae6e3ecc15
awb99 <awb99@gmail.com>
2017-02-15 07:05:51 +0100

    fedex cost estimateworking >150 lb in weight

--- testFedexCost.js
+++ testFedexCost.js
@@ -3,10 +3,14 @@ var fedex = require ("./fedexcost.js");
 
 function printInfo (clientZipCode, weight, clientInfo) {
     var data = fedex.fedexCost (clientZipCode, weight);
-    console.log (`distance to ${clientInfo} is: ${data.distance} miles; this is fedex Zone ${data.zone} for ${weight} lb the fedexGround Price is: ${data.price}.`);
+    console.log (`distance to ${clientInfo} is: ${data.distance} miles; this is fedex Zone ${data.zone} for ${weight} lb the fedexGround Price is: ${data.price}. USD/lb:${data.usdperlb}`);
 }
 
 
 fedex.readCSV( () => {
     printInfo (78230, 2.0, "Texas-SanAntonio");
+    printInfo (78230, 231.4, "Texas-SanAntonio"); // test multiple packets (150 lb + 81.4 lb)
+    printInfo (90049, 2.0, "LosAngeles BellAir");
+    printInfo (90049, 231.4, "LosAngeles BellAir");
 });
+
